f7832cfa31d2f11c091d539c30aa05dfe3283ede
Joakim Villo <villokodehode@gmail.com>
2023-03-29 14:24:53 +0200

    changed colors

--- src/components/Navigation/Header.jsx
+++ src/components/Navigation/Header.jsx
@@ -5,11 +5,11 @@ import JWC from "/JWC.png";
 
 export default function Header() {
   return (
-    <div className="absolute top-0 left-0 w-full bg-yellow-300 p-12">
+    <div className="absolute top-0 left-0 w-full bg-yellow-800 p-12">
       {/* <img src={JWC} className="absolute h-32" /> */}
       <ul className="text-xl">
         <li>
-          <Link to="/" className="shadow-xl border-4 border-yellow-200 p-4">
+          <Link to="/" className="shadow-xl border-4 border-yellow-700 p-4">
             Home
           </Link>
         </li>
--- src/components/Navigation/NavBlock.jsx
+++ src/components/Navigation/NavBlock.jsx
@@ -4,7 +4,7 @@ export default function NavBlock(props) {
   const { name, link } = props;
   return (
     <Link to={link}>
-      <div className="h-64 w-64 grid bg-yellow-300 items-center shadow-xl rounded-lg">
+      <div className="h-64 w-64 grid bg-yellow-700 items-center shadow-xl rounded-lg">
         <p className="text-3xl">{name}</p>
       </div>
     </Link>
